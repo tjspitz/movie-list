8dfc69f954991fb1e953b80e9b1cedfc12e7f51f
Tom Spitz <tjspitz1986@gmail.com>
2023-01-05 12:06:00 -0600

    Commit at end of level 0. Added styles to help me see the bits and pieces.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+const App = () => {
+  const movies = [
+    {title: 'Mean Girls'},
+    {title: 'Hackers'},
+    {title: 'The Grey'},
+    {title: 'Sunshine'},
+    {title: 'Ex Machina'},
+  ];
+
+
+  return (
+    <>
+      <h1 className="title">MovieList</h1>
+       <MovieList movies={movies}/>
+    </>
+  )
+
+};
+
+const MovieList = ({movies}) => {
+  return (
+    <div className="movie-list">
+      {movies.map(movie =><MovieListEntry movie={movie}/>)}
+    </div>
+  );
+
+};
+
+const MovieListEntry = ({movie}) => {
+  return (
+    <div className="movie-title">{movie.title}</div>
+  );
+
+};
 
 export default App;
