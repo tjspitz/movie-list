09a294d9e8432010a633b0d0e6e5c862ed51675d
Tom Spitz <tjspitz1986@gmail.com>
2023-01-14 17:47:17 -0600

    Left some notes at the bottom of the file.

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -12,10 +12,31 @@ const MovieList = ({movies}) => {
     <div className="movie-list">
       {!movies.length &&
       <div>Sorry, I couldn't find any movies that match your search...</div>}
-      {movies.map(movie =><MovieListEntry movie={movie}/>)}
+      {movies.map((movie, idx) => {
+        return <MovieListEntry key={idx} movie={movie}/>
+        })
+      }
     </div>
   )
 
 };
 
 export default MovieList;
+
+/*
+const MovieList = ({movies}) => {
+  // in react, && is like.... "then do this"...
+    // and if that condition fails, there is no ||, we just proceed to the mapping...
+
+  // but if we don't want to mess with that, we stick the ternary...
+    // return `right here ?` (...) `: and here`
+
+  return (
+    <div className="movie-list">
+      {!movies.length &&
+      <div>Sorry, I couldn't find any movies that match your search...</div>}
+      {movies.map(movie =><MovieListEntry movie={movie}/>)}
+    </div>
+  )
+};
+*/
